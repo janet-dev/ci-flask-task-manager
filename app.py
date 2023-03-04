1469b63e87845b1b4eca4cca263a77e821ee9056
JD <janet_dornan@hotmail.com>
2023-03-04 15:25:35 +0000

    Add Login functionality

--- app.py
+++ app.py
@@ -43,11 +43,41 @@ def register():
         }
         mongo.db.users.insert_one(register)
 
-        # put new user into 'session' cookie
+        # put new user into session cookie
         session["user"] = request.form.get("username").lower()
         flash("Registration successful!")
 
     return render_template("register.html")
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        # check if username exists in db
+        existing_user = mongo.db.users.find_one(
+            {"username": request.form.get("username").lower()})
+
+        if existing_user:
+            # ensure hashed password matches user input
+            if check_password_hash(
+                    existing_user["password"], request.form.get("password")):
+                session["user"] = request.form.get("username").lower()
+                flash("Welcome, {}".format(request.form.get("username")))
+            else:
+                # invalid password match
+                flash("Incorrect Username and/or Password")
+                return redirect(url_for("login"))
+
+        else:
+            # existing username does not exist
+            flash("Incorrect Username and/or Password")
+            return redirect(url_for("login"))
+
+    # Don't need to write any logic for an 'else' statement here
+    # it defaults to the "GET" method,
+    # which acts automatically as the 'else' condition
+
+    return render_template("login.html")
 
 
 if __name__ == "__main__":
