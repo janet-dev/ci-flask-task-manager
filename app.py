d6aac1adc7b4e1c154e51f69465aa4a35940ef21
JD <janet_dornan@hotmail.com>
2023-03-05 17:17:38 +0000

    Allow user to submit task to db

--- app.py
+++ app.py
@@ -107,8 +107,24 @@ def logout():
     return redirect(url_for("login"))
 
 
-@app.route("/add_task")
+@app.route("/add_task", methods=["GET", "POST"])
 def add_task():
+    if request.method == "POST":
+        is_urgent = "on" if request.form.get("is_urgent") else "off"
+        # set to 'on' if requested form element for 'is_urgent' is truthy
+        #  else, it will be set to 'off' by default
+        task = {
+            "category_name": request.form.get("category_name"),
+            "task_name": request.form.get("task_name"),
+            "task_description": request.form.get("task_description"),
+            "is_urgent": is_urgent,
+            "due_date": request.form.get("due_date"),
+            "created_by": session["user"]
+        }
+        mongo.db.tasks.insert_one(task)
+        flash("Task Successively Added")
+        return redirect(url_for("get_tasks"))
+
     categories = mongo.db.categories.find().sort("category_name")
     return render_template("add_task.html", categories=categories)
 
